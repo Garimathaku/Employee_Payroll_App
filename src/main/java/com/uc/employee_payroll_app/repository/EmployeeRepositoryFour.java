f1816aa4879a8c71b6296853638a2b07e92daf63
Garimathaku <garimathakur397@gmail.com>
2025-02-25 12:38:38 +0530

    Removed all files except README.md

--- src/main/java/com/uc/employee_payroll_app/repository/EmployeeRepositoryFour.java
+++
@@ -1,8 +0,0 @@
-package com.uc.employee_payroll_app.repository;
-
-import com.uc.employee_payroll_app.model.EmployeeFour;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepositoryFour extends JpaRepository<EmployeeFour, Long> {
-}
-
--- src/main/java/com/uc/employee_payroll_app/repository/EmployeeRepositoryThree.java
+++
@@ -1,8 +0,0 @@
-package com.uc.employee_payroll_app.repository;
-
-import com.uc.employee_payroll_app.model.EmployeeThree;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepositoryThree extends JpaRepository<EmployeeThree, Long> {
-}
-
